81861ba2791399545e715ee640709fe32966c07d
Nada0795 <nadakh1993@icloud.com>
2021-08-08 20:12:21 +0300

    adding backend to github

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const app = express();
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa'); 
+const PORT = process.env.PORT;
+const JWKSURI = process.env.JWKSURI;
+app.use(cors())
+
+const client = jwksClient({
+  jwksUri: JWKSURI
+});
+
+
+app.get('/', (request, response) => {
+  response.send('Hello World 🥳');
+});
+
+
+
+function getKey(header, callback) {
+  client.getSigningKey(header.kid, function (err, key) {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+
+
+
+app.get('/verify-token', (request, response) => {
+  
+  const token = request.headers.authorization.split(' ')[1];
+  console.log(token);
+  
+  jwt.verify(token, getKey, {}, (error, user) => {
+    if (error) {
+      response.send('invalid token');
+    }
+    response.json(user);
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
+
