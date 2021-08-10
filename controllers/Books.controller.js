c773c132dc334fc424d87b4e6e45b9e1bd73d429
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-08-10 18:40:39 +0300

    adding create and delete functionality on github

--- controllers/Books.controller.js
+++ controllers/Books.controller.js
@@ -9,4 +9,38 @@ const getBooks = async (req, res)=>{
     });
 
 };
-module.exports ={getBooks};
+
+const createBook = async (req, res) => {
+    // console.log("=======");
+    // To access the body data you will first need to decode the body data
+    // console.log(req.body);
+    // after decoding we need to assign the values to variables from the request body
+    const {
+      
+      title,
+      description,
+      status
+    } = req.body;
+  
+    // create the new cat 
+    const newBookObj = new userModel({
+        title: title,
+        description:description,
+        status:status,
+    });
+    newBookObj.save();
+  
+    res.json(newBookObj);
+  }
+
+  const deleteBook = async (req,res) =>{
+      const bookId = req.params.id;
+      userModel.deleteOne({_id:bookId},(error,deleted)=>{
+          res.send(deleted);
+      });
+  }
+module.exports ={getBooks,
+    createBook,
+    deleteBook
+
+};
